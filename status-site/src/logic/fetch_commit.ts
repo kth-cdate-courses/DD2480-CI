3e76eed70787400e0cc67ea76d130264b0e8f0bc
Hampus Hallkvist <hampus@hallkvist.org>
2023-02-07 18:42:59 +0100

    feat: Add design for commit page #23

--- status-site/src/logic/fetch_commit.ts
+++ status-site/src/logic/fetch_commit.ts
@@ -23,6 +23,7 @@ export async function fetchCommits(
     const bareCommit = bareCommits.find(
       (bareCommit) => bareCommit.sha === commit.sha
     )
+    console.log(bareCommit?.log)
     return {
       ...commit,
       status: bareCommit?.status ?? "unset",
