0f2a441602f7a635c8210ea307b17fe40c3e306b
Storsorken <erikwac@kth.se>
2023-02-07 23:15:45 +0100

    test: issue #20 add test for test runner method
    Test tasks test-runner method to compile a dummy project and write the results to temp file. Then checks that temp file isn't empty, i.e. tests ran and output was written to file.

--- ci-server/src/test/java/com/group1/AppTest.java
+++ ci-server/src/test/java/com/group1/AppTest.java
@@ -2,6 +2,8 @@ package com.group1;
 
 import static org.junit.Assert.assertTrue;
 
+import java.io.File;
+
 import org.junit.Test;
 
 /**
@@ -16,5 +18,18 @@ public class AppTest
     public void shouldAnswerWithTrue()
     {
         assertTrue( true );
+    }
+
+    @Test
+    public void output_from_compileAndRunTests_is_written_to_file() {
+        File targetDir = new File("testing_resources/maven_project_for_unit_testing");
+        File outputFile = new File("testing_resources/tempUnitTestFile");
+
+        if (outputFile.exists())
+            outputFile.delete();
+            
+        new ContinuousIntegrationServer().compileAndRunTests(targetDir, outputFile);
+        
+        assertTrue(outputFile.length() > 0);
     }
 }
