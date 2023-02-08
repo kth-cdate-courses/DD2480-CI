52d9df4875797624f7b66afa2cf96df314ce7310
Storsorken <erikwac@kth.se>
2023-02-08 23:48:21 +0100

    test: issue #20 add tests
    Test that target directory is created and that IllegalArgumentException is thrown.

--- ci-server/src/test/java/com/group1/AppTest.java
+++ ci-server/src/test/java/com/group1/AppTest.java
@@ -32,4 +32,26 @@ public class AppTest
         
         assertTrue(outputFile.length() > 0);
     }
+
+    @Test
+    public void compileAndRunTests_creates_directory_called_target_for_well_formed_maven_project() {
+        File targetDir = new File("testing_resources/maven_project_for_unit_testing");
+        File outputFile = new File("testing_resources/tempUnitTestFile");
+
+        if (outputFile.exists())
+            outputFile.delete();
+            
+        new ContinuousIntegrationServer().compileAndRunTests(targetDir, outputFile);
+        
+        File maven_target_dir = new File(targetDir, "target");
+        assertTrue(maven_target_dir.exists());
+    }
+
+    @Test (expected = IllegalArgumentException.class)
+    public void compileAndRunTests_throws_IllegalArgumentException_for_non_maven_Directory() {
+        File targetDir = new File("testing_resources");
+        File outputFile = new File("testing_resources/tempUnitTestFile");
+
+        new ContinuousIntegrationServer().compileAndRunTests(targetDir, outputFile);
+    }
 }
