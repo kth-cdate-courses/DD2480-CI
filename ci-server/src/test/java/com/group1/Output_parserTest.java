b2776c6463f692ef7a3b2a678657ac82d698ea58
EdwardNorberg <norberg.edward@gmail.com>
2023-02-09 12:30:48 +0100

    refactor: issue #30 move txt to testing_recourses
    move test txt-files to resting recourses folder.

--- ci-server/src/test/java/com/group1/Output_parserTest.java
+++ ci-server/src/test/java/com/group1/Output_parserTest.java
@@ -11,27 +11,27 @@ public class Output_parserTest {
     //the enum Status.COMPILE_FAILED
     @Test
     public void compileFail() throws FileParsingFailedException {
-        assertTrue(Output_parser.output_file_state_parser(new File("compileFail.txt")) == Status.COMPILE_FAILED);
+        assertTrue(Output_parser.output_file_state_parser(new File("testing_resources/compileFail.txt")) == Status.COMPILE_FAILED);
     }
 
     //Testing of a file which contains the output message of a test failure. method output_file_state_parser/1 should return
     //the enum Status.TEST_FAILED
     @Test
     public void testFail() throws FileParsingFailedException {
-        assertTrue(Output_parser.output_file_state_parser(new File("testFail.txt")) == Status.TEST_FAILED);
+        assertTrue(Output_parser.output_file_state_parser(new File("testing_resources/testFail.txt")) == Status.TEST_FAILED);
     }
 
     //Testing of a file which contains the output message of successful build and testing.
     //method output_file_state_parser/1 should return the enum Status.SUCCESS
     @Test
     public void testSuccess() throws FileParsingFailedException {
-        assertTrue(Output_parser.output_file_state_parser(new File("testSuccess.txt")) == Status.SUCCESS);
+        assertTrue(Output_parser.output_file_state_parser(new File("testing_resources/testSuccess.txt")) == Status.SUCCESS);
     }
 
     //Testing of a file which does not exist. method output_file_state_parser/1 should throw FileParsingFailedException
     @Test(expected = FileParsingFailedException.class)
     public void nonexistentFile() throws FileParsingFailedException {
-        Output_parser.output_file_state_parser(new File("nonexistentFile.txt"));
+        Output_parser.output_file_state_parser(new File("testing_resources/nonexistentFile.txt"));
     }
 
     //Testing of a file which contains the output message of successful build and testing.
@@ -39,7 +39,7 @@ public class Output_parserTest {
     //which should be not equal to the enum Status.COMPILE_FAILED
     @Test
     public void falseConclusion() throws FileParsingFailedException {
-        assertFalse(Output_parser.output_file_state_parser(new File("testSuccess.txt")) == Status.COMPILE_FAILED);
+        assertFalse(Output_parser.output_file_state_parser(new File("testing_resources/testSuccess.txt")) == Status.COMPILE_FAILED);
     }
 
 }
