f576a3f75988030e9debb7d83c793e1b027beff7
EdwardNorberg <norberg.edward@gmail.com>
2023-02-09 03:44:55 +0100

    fix: issue #30 fix unit tests to handle exceptions
    for unit tests of method output_file_state_parser

--- ci-server/src/test/java/com/group1/Output_parserTest.java
+++ ci-server/src/test/java/com/group1/Output_parserTest.java
@@ -10,37 +10,36 @@ public class Output_parserTest {
     //Testing of a file which contains the output message of compile failure. method output_file_state_parser/1 should return
     //the enum Status.COMPILE_FAILED
     @Test
-    public void compileFail() {
+    public void compileFail() throws FileParsingFailedException {
         assertTrue(Output_parser.output_file_state_parser(new File("compileFail.txt")) == Status.COMPILE_FAILED);
     }
 
     //Testing of a file which contains the output message of a test failure. method output_file_state_parser/1 should return
     //the enum Status.TEST_FAILED
     @Test
-    public void testFail() {
+    public void testFail() throws FileParsingFailedException {
         assertTrue(Output_parser.output_file_state_parser(new File("testFail.txt")) == Status.TEST_FAILED);
     }
 
     //Testing of a file which contains the output message of successful build and testing.
     //method output_file_state_parser/1 should return the enum Status.SUCCESS
     @Test
-    public void testSuccess() {
+    public void testSuccess() throws FileParsingFailedException {
         assertTrue(Output_parser.output_file_state_parser(new File("testSuccess.txt")) == Status.SUCCESS);
     }
 
-    //Testing of a file which does not exist. method output_file_state_parser/1 should return
-    //the enum Status.DOWNLOAD_FAILED
-    @Test
-    public void nonexistentFile() {
-        assertTrue(Output_parser.output_file_state_parser(new File("nonexistentFile.txt")) == Status.DOWNLOAD_FAILED);
+    //Testing of a file which does not exist. method output_file_state_parser/1 should throw FileParsingFailedException
+    @Test(expected = FileParsingFailedException.class)
+    public void nonexistentFile() throws FileParsingFailedException {
+        Output_parser.output_file_state_parser(new File("nonexistentFile.txt"));
     }
 
     //Testing of a file which contains the output message of successful build and testing.
     //method output_file_state_parser/1 should return the enum Status.SUCCESS,
-    //which should be not equal to the enum Status.DOWNLOAD_FAILED
+    //which should be not equal to the enum Status.COMPILE_FAILED
     @Test
-    public void falseConclusion() {
-        assertFalse(Output_parser.output_file_state_parser(new File("testSuccess.txt")) == Status.DOWNLOAD_FAILED);
+    public void falseConclusion() throws FileParsingFailedException {
+        assertFalse(Output_parser.output_file_state_parser(new File("testSuccess.txt")) == Status.COMPILE_FAILED);
     }
 
 }
