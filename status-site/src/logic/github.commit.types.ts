cc889aad1114fb79174ba9a35d5535513a0889b2
Hampus Hallkvist <hampus@hallkvist.org>
2023-02-09 11:41:37 +0100

    fix: #14 fetch contributors

--- status-site/src/logic/github.commit.types.ts
+++ status-site/src/logic/github.commit.types.ts
@@ -2,6 +2,31 @@ import type { BareCommit } from "./fetch_commit"
 
 export type ExtendedCommit = Commit & BareCommit
 
+export interface Contributor {
+  login?: string
+  id?: number
+  node_id?: string
+  avatar_url?: string
+  gravatar_id?: string | null
+  url?: string
+  html_url?: string
+  followers_url?: string
+  following_url?: string
+  gists_url?: string
+  starred_url?: string
+  subscriptions_url?: string
+  organizations_url?: string
+  repos_url?: string
+  events_url?: string
+  received_events_url?: string
+  type: string
+  site_admin?: boolean
+  contributions: number
+  email?: string
+  name?: string
+  [k: string]: unknown
+}
+
 export interface Commit {
   url: string
   sha: string
