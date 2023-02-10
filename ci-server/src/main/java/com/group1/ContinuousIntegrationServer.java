54ddf80aae91967831b79ac91adf95a921eedf35
Storsorken <erikwac@kth.se>
2023-02-10 14:55:00 +0100

    feat: issue #34 add appendCommit method

--- ci-server/src/main/java/com/group1/ContinuousIntegrationServer.java
+++ ci-server/src/main/java/com/group1/ContinuousIntegrationServer.java
@@ -11,6 +11,10 @@ import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.Request;
 import org.eclipse.jetty.server.handler.AbstractHandler;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+
 /**
  * Skeleton of a ContinuousIntegrationServer which acts as webhook
  * See the Jetty documentation for API documentation of those classes.
@@ -18,7 +22,7 @@ import org.eclipse.jetty.server.handler.AbstractHandler;
 public class ContinuousIntegrationServer extends AbstractHandler {
     public void handle(String target, Request baseRequest, HttpServletRequest request, HttpServletResponse response)
             throws IOException, ServletException {
-        
+
         response.setContentType("text/html;charset=utf-8");
         response.setStatus(HttpServletResponse.SC_OK);
         baseRequest.setHandled(true);
@@ -58,6 +62,32 @@ public class ContinuousIntegrationServer extends AbstractHandler {
         } catch (IOException e) {
             e.printStackTrace();
         } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Tries to append a commit to a json file with a list of commits.
+     * 
+     * It is assumed that the json file contains a list tagged "commits" in the top
+     * level.
+     * 
+     * @param file   json file
+     * @param commit commit to be appended
+     * @throws IllegalArgumentException Must be a .json file
+     */
+    void appendCommit(File file, Commit commit) throws IllegalArgumentException {
+        boolean isJsonFile = file.getName().endsWith(".json");
+        if (!file.exists() || !isJsonFile)
+            throw new IllegalArgumentException("Not a json file");
+
+        try {
+            ObjectMapper mapper = new ObjectMapper();
+            JsonNode dataFile = mapper.readTree(file);
+            ArrayNode commits = (ArrayNode) dataFile.get("commits");
+            commits.addPOJO(commit);
+            mapper.writeValue(file, dataFile);
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
