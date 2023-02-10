ec0faee579c6f405819024a89c18b0c623a5d74e
Pauline Garelli <pauline.garelli@hotmail.fr>
2023-02-10 10:55:05 +0100

    feat: first test for repository cloning #27

--- ci-server/src/test/java/com/group1/AppTest.java
+++ ci-server/src/test/java/com/group1/AppTest.java
@@ -1,20 +1,34 @@
 package com.group1;
 
-import static org.junit.Assert.assertTrue;
+import java.net.URL;
+import java.io.File;
+import java.net.MalformedURLException;
 
+import static org.junit.Assert.assertTrue;
 import org.junit.Test;
 
 /**
- * Unit test for simple App.
+ * Unit testing for the ContinuousIntegrationServer class
  */
 public class AppTest 
 {
-    /**
-     * Rigorous Test :-)
+     /**
+     * Tests that the repository where we cloned the git project is not empty
      */
     @Test
-    public void shouldAnswerWithTrue()
+    public void repositoryNotEmptyCloningTest()
     {
-        assertTrue( true );
+        try{
+        URL repoUrl = new URL("git@github.com:kth-cdate-courses/DD2480-CI.git");
+        File repoDirectory = new File("./watched-repository");
+        File outputFile = new File("./dowloadOutput.txt");
+
+        ContinuousIntegrationServer.cloneRepository(repoUrl, repoDirectory, outputFile);
+        String[] files = repoDirectory.list();
+        assertTrue(files.length > 0);
+        }
+        catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
     }
 }
